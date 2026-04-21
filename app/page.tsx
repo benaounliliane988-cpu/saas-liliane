db9068b62c16a1db86e0ac82322de26e64f3262b
Liliane <benaounliliane988@gmail.com>
2026-04-21 19:13:44 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Welcome to liliane Saas! hopefully u love it </h1>
+     <Button > Let's start !</Button>
+    </div>
+  )
+}
+
+export default Page
